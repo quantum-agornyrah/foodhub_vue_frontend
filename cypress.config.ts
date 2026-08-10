1406bbdf9a8fb94a02b070f2754f5f5c67f37827
Agornyrah Eric <eagornyrah@quantumgroupgh.com>
2026-08-10 16:09:18 -0700

    refactor: cypress element interraction

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // Cypress scrollBehavior accepts: 'center', 'top', 'bottom', 'nearest', false
+  scrollBehavior: 'center',
+
   allowCypressEnv: false,
   watchForFileChanges: false,
 
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
