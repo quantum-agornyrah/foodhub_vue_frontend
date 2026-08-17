1e11ce99a2ecf35284f01af130fef9ae22c52c9f
Agornyrah Eric <eagornyrah@quantumgroupgh.com>
2026-08-17 16:47:30 -0700

    refactor: add staff workflow e2e tests

---
+++ cypress/e2e/staff-test.cy.js
@@ -0,0 +1,56 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.StaffLogin('cypresstester@gmail.com', 'cypress123')
+  })
+
+  it('Render necessary data on the dashboard', () => {
+    // Declare all necessary intercepts
+    cy.intercept('GET', '**/staff/all/**').as('getAllStaff')
+    cy.intercept('GET', '**/menu/all/**').as('getMenu')
+
+    cy.visit('/staff')
+    cy.url().should('include', '/staff')
+    cy.contains('Good', { timeout: 10000 }).should('be.visible')
+  })
+
+  it('Navigate from dashboard to History page', () => {
+    // Get all menu items
+    cy.intercept('GET', '**/orders/all/**').as('getAllOrders')
+
+    cy.visit('/staff')
+    cy.url().should('include', '/staff')
+
+    // Find the History button and click it
+    cy.contains('button', 'View All History').click()
+    cy.url().should('include', '/staff/history')
+    cy.contains('My order history').should('be.visible')
+  })
+
+  it('Navigate to the overview page, select and make an order', () => {
+    // Get all order items and make an api call for selected orders 
+    cy.intercept('GET', '**/staff/orders/**').as('getOrders')
+    cy.intercept('POST', '**/staff/orders/**').as('postOrder')
+
+    cy.visit('/staff')
+    cy.url().should('include', '/staff')
+
+    // Click the image link of each item card
+    cy.get('.clickable-image').first().click()
+    cy.url().should('include', 'overview')
+    
+    cy.contains('Order for Next Week').should('be.visible')
+
+    // Make a selection in the first day card
+    cy.get('.cursor-pointer').first().click()
+
+    // Submit selections
+    cy.contains('button', 'Submit all selections').should('not.be.disabled').click()
+
+    // Wait for the api call to return a success message that an order has been saved
+    cy.wait('@postOrder').its('response.statusCode').should('be.oneOf', [200, 201])
+
+    cy.visit('/staff/history')
+    cy.url().should('include', '/staff/history')
+    cy.get('.v-data-table', { timeout: 10000 }).should('contain', 'Submitted')
+  })
+})
